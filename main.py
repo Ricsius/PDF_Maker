c79307005cc54233a1b9c8e343bbfe18300b26f0
Ricsius <hajnal.ricsi@gmail.com>
2024-11-11 12:53:25 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from fpdf import FPDF
+import pandas as pd
+
+PAGES_KEY = "Pages"
+TOPIC_KEY = "Topic"
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+data = pd.read_csv("topics.csv")
+
+for index, row in data.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row[TOPIC_KEY], align="L", ln=1)
+    pdf.line(10, 21, 200, 21)
+
+pdf.output("output.pdf")
